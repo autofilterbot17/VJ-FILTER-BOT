2df5486f5338128d0e3535938fc86a10d027f3ed
autofilterbot17 <autofilterbot17@gmail.com>
2025-02-16 08:11:45 +0530

    Update info.py

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/srm_request_movie')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/srm_movies')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'srm_request_movie') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/rahul_srm_official')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/SRM_Movies_Owner_bot')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
